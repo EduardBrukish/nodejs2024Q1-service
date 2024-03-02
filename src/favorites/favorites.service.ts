6896cb720ad16d931699e00ca02c33f165189b12
Eduard Brukish <eduardbrukish@gmail.com>
2024-03-02 20:39:51 +0200

    feat(develop): Add user/album/track/artist/favorites basic models

---
+++ src/favorites/favorites.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+
+export class FavoritesService {
+  getFavorites(): string {
+    return 'Now you have all favorites albums'
+  }
+}
