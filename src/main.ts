aeb1305a9ab43431f0631f84c9495ef2386cf2e7
Eduard Brukish <eduardbrukish@gmail.com>
2024-03-06 10:55:25 +0200

    feat(develop): add logic to use global variables from env file

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+console.log(process.env.PORT)
+const PORT = process.env.PORT || 4000
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(PORT);
 }
 bootstrap();
