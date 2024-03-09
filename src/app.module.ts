45d7c4b99dacdd91ba369275f66c29208cbcba33
Eduard Brukish <eduardbrukish@gmail.com>
2024-03-09 16:29:21 +0200

    feat(containerization-db) add type orm to the project and setup  project to use db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -16,6 +17,18 @@ import { FavoritesModule } from './favorites/favorites.module';
     AlbumModule,
     FavoritesModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+      parseInt8: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
